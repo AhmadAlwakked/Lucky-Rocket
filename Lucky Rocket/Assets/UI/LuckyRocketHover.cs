50aa2881ee5c86a14bf26eac98e691750395bc24
AhmadAlwakked <alwakedahmad6@gmail.com>
2026-08-24 14:00:13 +0200

    main menu text
    This reverts commit 28e35e397b334d95061fd6477f503135bcc56230, reversing
    changes made to aa62d0905978964871de5f90aa904d4370aa4ea6.

--- Lucky Rocket/Assets/UI/LuckyRocketHover.cs
+++ Lucky Rocket/Assets/UI/LuckyRocketHover.cs
@@ -4,6 +4,10 @@ using TMPro;
 
 public class LuckyRocketHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
+    [Header("Text Curve")]
+    [Range(-100f, 100f)]
+    public float curveAmount = 20f;
+
     [Header("Hover Effect")]
     public float hoverScale = 1.08f;
     public float animationSpeed = 8f;
@@ -26,7 +30,8 @@ public class LuckyRocketHover : MonoBehaviour, IPointerEnterHandler, IPointerExi
 
     void Update()
     {
-        // Scale effect
+        ApplyCurve();
+
         Vector3 targetScale = isHovering
             ? originalScale * hoverScale
             : originalScale;
@@ -37,7 +42,6 @@ public class LuckyRocketHover : MonoBehaviour, IPointerEnterHandler, IPointerExi
             Time.deltaTime * animationSpeed
         );
 
-        // Color effect
         Color targetColor = isHovering
             ? hoverColor
             : normalColor;
@@ -47,6 +51,51 @@ public class LuckyRocketHover : MonoBehaviour, IPointerEnterHandler, IPointerExi
             targetColor,
             Time.deltaTime * animationSpeed
         );
+    }
+
+    void ApplyCurve()
+    {
+        text.ForceMeshUpdate();
+
+        TMP_TextInfo textInfo = text.textInfo;
+
+        float centerX = text.bounds.center.x;
+
+        for (int i = 0; i < textInfo.characterCount; i++)
+        {
+            TMP_CharacterInfo charInfo = textInfo.characterInfo[i];
+
+            if (!charInfo.isVisible)
+                continue;
+
+            int vertexIndex = charInfo.vertexIndex;
+            int materialIndex = charInfo.materialReferenceIndex;
+
+            Vector3[] vertices =
+                textInfo.meshInfo[materialIndex].vertices;
+
+            for (int j = 0; j < 4; j++)
+            {
+                Vector3 pos = vertices[vertexIndex + j];
+
+                float x = pos.x - centerX;
+
+                pos.y += (x * x) * curveAmount * 0.001f;
+
+                vertices[vertexIndex + j] = pos;
+            }
+        }
+
+        for (int i = 0; i < textInfo.meshInfo.Length; i++)
+        {
+            textInfo.meshInfo[i].mesh.vertices =
+                textInfo.meshInfo[i].vertices;
+
+            text.UpdateGeometry(
+                textInfo.meshInfo[i].mesh,
+                i
+            );
+        }
     }
 
     public void OnPointerEnter(PointerEventData eventData)
